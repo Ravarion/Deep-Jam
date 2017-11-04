1a30f383135af10eaf23fe725eb9de77634758fb
Scoopadoop <giesfeldte0427@uwstout.edu>
2017-11-04 18:34:15 -0500

    added scene change for revenge game
    we need a "rotate screen for revenge" image instead of the title screen
    image lol
    
    it would also look good if it was an animation that blinked on and off,
    like an old arcade continue screen

--- Assets/Scripts/CheckOrientation.cs
+++ Assets/Scripts/CheckOrientation.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CheckOrientation : MonoBehaviour {
+
+	public GameObject deadShark;
 
 	// Use this for initialization
 	void Start () {
@@ -11,7 +14,11 @@ public class CheckOrientation : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.deviceOrientation == DeviceOrientation.LandscapeLeft)
-			SceneManager.LoadScene("RevengeGame");
+		if (deadShark.transform.position.y <= 0) {	
+			if (transform.GetComponent<SpriteRenderer> ().enabled == false)
+				transform.GetComponent<SpriteRenderer> ().enabled = true;
+			if (Input.deviceOrientation == DeviceOrientation.LandscapeLeft)
+				SceneManager.LoadScene ("RevengeGame");
+		}
 	}
 }

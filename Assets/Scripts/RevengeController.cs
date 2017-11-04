e49ba08e7e914e8ff33abdcb08a965488c559449
Scoopadoop <giesfeldte0427@uwstout.edu>
2017-11-04 18:05:03 -0500

    Revenge game is working somewhat
    Still working on boat spawning

---
+++ Assets/Scripts/RevengeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RevengeController : MonoBehaviour {
+
+    public GameObject sharkObject;
+
+    private void Start()
+    {
+        
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector2 touchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (true)
+        {
+			if(touchPosition.y > 1.5f)
+				sharkObject.GetComponent<Shark>().desiredPosition = new Vector2(touchPosition.x, 1.5f);
+			else
+				sharkObject.GetComponent<Shark>().desiredPosition = new Vector2(touchPosition.x, touchPosition.y);
+        }
+    }
+}
